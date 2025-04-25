e29d75551db5b0760e69149e7df94b34bed94503
Gabriel Reyes <gabriel@handy.la>
2025-04-24 18:47:18 -0600

    Se agregan casos de prueba para productos via Cypress

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -19,11 +19,19 @@ environment.plugins.prepend(
 
 // Disable compression during asset precompilation
 if (process.env.RAILS_ENV === 'production' && process.env.NODE_ENV === 'production') {
-  try {
-    environment.plugins.delete('CompressionPlugin')
-  } catch (e) {
-    // Ignore if plugin doesn't exist
-  }
+  // Remove all compression-related plugins
+  const pluginsToRemove = ['CompressionPlugin', 'compression-webpack-plugin']
+  pluginsToRemove.forEach(pluginName => {
+    try {
+      environment.plugins.delete(pluginName)
+    } catch (e) {
+      // Ignore if plugin doesn't exist
+    }
+  })
+  
+  // Disable compression in webpack config
+  environment.config.optimization = environment.config.optimization || {}
+  environment.config.optimization.minimize = false
 }
 
 module.exports = environment
