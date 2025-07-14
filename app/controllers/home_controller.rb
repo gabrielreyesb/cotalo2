04277fcb64925529168422c59c6f4ab2309f9dc0
Gabriel Reyes <gabriel@handy.la>
2025-07-14 12:54:48 -0600

    Simplify dashboard: keep only welcome message, recent quotes and recent products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,21 +11,6 @@ class HomeController < ApplicationController
     if @user.trial_ends_at.nil? && @user.subscription_status == 'trial'
       @user.update(trial_ends_at: 14.days.from_now)
     end
-    
-    # Quote metrics
-    @quotes = @user.quotes
-    @total_quotes_value = @quotes.sum { |q| q.data&.dig('totals', 'total') || 0 }
-    @average_quote_value = @quotes.any? ? @total_quotes_value / @quotes.count : 0
-    @recent_quotes_count = @quotes.where('created_at >= ?', 30.days.ago).count
-    @highest_quote = @quotes.max_by { |q| q.data&.dig('totals', 'total') || 0 }
-    
-    # Product metrics
-    @products = @user.products
-    @highest_value_products = @products
-      .select { |p| p.data&.dig('pricing', 'total_price') }
-      .sort_by { |p| p.data&.dig('pricing', 'total_price') || 0 }
-      .reverse
-      .first(5)
   end
 
   def about
